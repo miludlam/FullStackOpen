96b527295ce7bcdadc4208eef872bc9d125a8d41
Michael Ludlam <michael@realtechvr.com>
2025-01-11 21:57:24 -0500

    Committing exercises 2.2-2.5

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -5,10 +5,21 @@ const Content = ({ parts }) => (
         {parts.map(part =>
             <Part key={part.id} name={part.name} exercises={part.exercises} />
         )}
+        <Total exercises={parts.map(part => part.exercises)} />
     </div>
 )
 
 const Part = ({ name, exercises }) => <p>{name} {exercises}</p>
+
+const Total = ({ exercises }) => {
+    const total = exercises.reduce(
+        (acc, current) => acc + current,
+        0,
+    );
+    return (
+        <p><strong>total of {total} exercises</strong></p>
+    )
+}
 
 const Course = ({ course }) => {
     return (
