31e5d2efc532cb4bc2c50aacf1fd03326c9a4f22
Michael Ludlam <miludlam@gmail.com>
2025-01-17 22:30:57 -0500

    Adding number field to form and handling in Person object

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,13 +3,14 @@ import Person from './components/Person'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas' }
+        { name: 'Arto Hellas', number: '040-1234567' }
     ])
     const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
 
-    const addName = (event) => {
+    const addPerson = (event) => {
         event.preventDefault()
-        const nameObject = { name: newName }
+        const personObject = { name: newName, number: newNumber }
 
         let nameFound = false
 
@@ -21,21 +22,29 @@ const App = () => {
         })
 
         if (!nameFound) {
-            setPersons(persons.concat(nameObject))
+            setPersons(persons.concat(personObject))
         }
         setNewName('')
+        setNewNumber('')
     }
 
     const handleNameChange = (event) => {
         setNewName(event.target.value)
     }
 
+    const handleNumberChange = (event) => {
+        setNewNumber(event.target.value)
+    }
+
     return (
         <div>
             <h2>Phonebook</h2>
-            <form onSubmit={addName}>
+            <form onSubmit={addPerson}>
                 <div>
                     name: <input value={newName} onChange={handleNameChange} />
+                </div>
+                <div>
+                    number: <input value={newNumber} onChange={handleNumberChange} />
                 </div>
                 <div>
                     <button type="submit">add</button>
