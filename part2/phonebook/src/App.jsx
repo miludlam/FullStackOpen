4ca14eaba4b118dff7a169c6587a6defb74bd6d2
Michael Ludlam <michael@realtechvr.com>
2025-01-23 18:29:41 -0500

    Extracting filter, persons, and form components 2.10

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,17 +1,55 @@
 import { useState } from 'react'
 import Person from './components/Person'
 
+const Filter = ({ personFilter, handleNameFilter }) => {
+    return (
+        <div>
+            filter shown with <input value={personFilter} onChange={handleNameFilter} />
+        </div>
+    )
+}
+
+const PersonForm = ({ addPerson, newName, handleNameChange, newNumber, handleNumberChange }) => {
+    return (
+        <form onSubmit={addPerson}>
+            <div>
+                name: <input value={newName} onChange={handleNameChange} />
+            </div>
+            <div>
+                number: <input value={newNumber} onChange={handleNumberChange} />
+            </div>
+            <div>
+                <button type="submit">add</button>
+            </div>
+        </form>
+    )
+}
+
+const Persons = ({filteredPersons}) => {
+    return (
+        <div>
+            {filteredPersons.map(person =>
+                <Person key={person.name} person={person} />
+            )}
+        </div>
+    )
+}
+
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-1234567' }
+        { name: 'Arto Hellas', number: '040-123456', id: 1 },
+        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
     ])
+
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [personFilter, setPersonFilter] = useState('')
 
     const addPerson = (event) => {
         event.preventDefault()
-        const personObject = { name: newName, number: newNumber }
+        const personObject = { name: newName, number: newNumber, id: persons.length + 1 }
 
         const matches = persons.filter((person) => person.name === newName)
 
@@ -43,27 +81,22 @@ const App = () => {
     return (
         <div>
             <h2>Phonebook</h2>
-            <div>
-                filter shown with <input value={personFilter} onChange={handleNameFilter} />
-            </div>
+            <Filter
+                personFilter={personFilter}
+                handleNameFilter={handleNameFilter}
+            />
             <h3>Add a new</h3>
-            <form onSubmit={addPerson}>
-                <div>
-                    name: <input value={newName} onChange={handleNameChange} />
-                </div>
-                <div>
-                    number: <input value={newNumber} onChange={handleNumberChange} />
-                </div>
-                <div>
-                    <button type="submit">add</button>
-                </div>
-            </form>
+            <PersonForm
+                addPerson={addPerson}
+                newName={newName}
+                handleNameChange={handleNameChange}
+                newNumber={newNumber}
+                handleNumberChange={handleNumberChange}
+            />
             <h3>Numbers</h3>
-            <div>
-                {filteredPersons.map(person =>
-                    <Person key={person.name} person={person} />
-                )}
-            </div>
+            <Persons
+                filteredPersons={filteredPersons}
+            />
         </div>
     )
 }
